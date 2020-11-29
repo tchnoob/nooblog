f083627210a95969cce0954ffb841af66872dd34
sj <sjun_oh@naver.com>
2020-11-29 23:24:44 +0900

    View에서 조회 후 card list 출력

--- src/view/HomeView.js
+++ src/view/HomeView.js
@@ -1,8 +1,24 @@
 import React, { Component } from "react";
-
+import repository from "../repository/Repository";
+import { PostCardList } from "../component";
 class HomeView extends Component {
+  state = {
+    posts: [],
+  };
+
+  componentDidMount() {
+    repository
+      .getRecentPost(7)
+      .then(({ data: posts }) => this.setState({ posts }));
+  }
+
   render() {
-    return <div>home</div>;
+    return (
+      <div>
+        home
+        <PostCardList posts={this.state.posts || []} />
+      </div>
+    );
   }
 }
 
--- src/view/PostView.js
+++ src/view/PostView.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+import repository from "../repository/Repository";
 
 const PostView = (props) => {
+  const [post, setContent] = useState(null);
   const { query } = props.match.params;
-  return <div>Post : {query}</div>;
+  if (!post)
+    repository.getPostById(query).then(({ data }) => setContent(data[0]));
+  return (
+    <div>
+      Post : {query}
+      <div>{post && post.content}</div>
+    </div>
+  );
 };
 
 export default PostView;
