4fbb5cfa86f9f243931adc759d61632797d4d7cb
sj <sjun_oh@naver.com>
2020-11-29 18:47:01 +0900

    빌드 배포

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -4,7 +4,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "bundle.[hash].js",
+    filename: "[name].bundle.js",
     publicPath: "/",
   },
   devServer: {
