277c1a6af7ac070c0b5611f7c57a30640541ea15
sj <sjun_oh@naver.com>
2020-11-29 19:27:47 +0900

    decorator 제외

--- src/store/Store.js
+++ src/store/Store.js
@@ -2,39 +2,37 @@ import { makeObservable, action, observable } from "mobx";
 import { boundClass } from "autobind-decorator";
 import _ from "lodash";
 
-@boundClass
-class Store {
-  @observable
-  title = "";
-  @observable
-  menuVisible = true;
+const Store = boundClass(
+  class {
+    title = "";
+    menuVisible = true;
 
-  constructor() {
-    this.onScroll = _.debounce((e) => {
-      if (this.menuVisible) {
-        this.menuVisible = false;
-      } else if (e.target.scrollTop === 0) {
-        this.menuVisible = true;
-      }
-      console.log("@onScroll", e, this.menuVisible);
-    }, 50);
-    makeObservable(this);
+    constructor() {
+      this.onScroll = _.debounce((e) => {
+        if (this.menuVisible) {
+          this.menuVisible = false;
+        } else if (e.target.scrollTop === 0) {
+          this.menuVisible = true;
+        }
+        console.log("@onScroll", e, this.menuVisible);
+      }, 33);
+      makeObservable(this, {
+        title: observable,
+        menuVisible: observable,
+      });
+    }
+
+    setTitle(title) {
+      this.title = title;
+      console.log("@setTitle", this.title);
+    }
+
+    toggleMenuView() {
+      this.menuVisible = !this.menuVisible;
+      console.log("@toggleMenuView", this.menuVisible);
+    }
   }
-
-  setTitle(title) {
-    this.title = title;
-    console.log("@setTitle", this.title);
-  }
-
-  toggleMenuView() {
-    this.menuVisible = !this.menuVisible;
-    console.log("@toggleMenuView", this.menuVisible);
-  }
-
-  // onScroll(e) {
-
-  // }
-}
+);
 
 const store = new Store();
 export default Store;
