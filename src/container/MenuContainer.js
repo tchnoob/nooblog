aea869362ed5c8c2e97ec9216ccb0a7a2419f649
sj <sjun_oh@naver.com>
2020-12-02 23:43:21 +0900

    로그인 사용자 function panel 추가

--- src/container/MenuContainer.js
+++ src/container/MenuContainer.js
@@ -1,16 +1,30 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import CreateOutlinedIcon from "@material-ui/icons/CreateOutlined";
+import Divider from "@material-ui/core/Divider";
+import { withAuth0 } from "@auth0/auth0-react";
+import "../layout/style.css";
+
 class MenuContainer extends Component {
   render() {
+    const { isAuthenticated } = this.props.auth0;
     return (
-      <Fragment>
-        <Link to="/공부">공부</Link>
-        <Link to="/리뷰">리뷰</Link>
-        <Link to="/스크랩">스크랩</Link>
-        <Link to="/이것저것">이것저것</Link>
-      </Fragment>
+      <div className="menu-container">
+        <div className="menu section">
+          <Link to="/공부">공부</Link>
+          <Link to="/리뷰">리뷰</Link>
+          <Link to="/스크랩">스크랩</Link>
+          <Link to="/이것저것">이것저것</Link>
+        </div>
+        <div
+          className="function section"
+          style={{ display: !isAuthenticated ? "block" : "none" }}
+        >
+          <CreateOutlinedIcon />
+        </div>
+      </div>
     );
   }
 }
 
-export default MenuContainer;
+export default withAuth0(MenuContainer);
